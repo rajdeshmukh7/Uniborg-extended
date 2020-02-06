5d04119166b119fa39a455208dcc031a40303679
rajdeshmukh7 <59872124+rajdeshmukh7@users.noreply.github.com>
2020-02-06 21:01:05 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`Your bot is running ｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\n"
-                     "`The King of bots /n"
-                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/rajdeshmukh7/Uniborg-extended)")
+    await alive.edit("**`𝕀 𝕒𝕞 𝕒𝕝𝕚𝕧𝕖 𝕞𝕪 𝕞𝕒𝕤𝕥𝕖𝕣`**\n\n"
+                     "`𝗧𝗲𝗹𝗲𝘁𝗵𝗼𝗻 𝘃𝗲𝗿𝘀𝗶𝗼𝗻: 6.9.0\nPython: 3.7.3\n\n"
+                     "`𝗗𝗮𝘁𝗮𝗯𝗮𝘀𝗲 𝘀𝘁𝗮𝘁𝘂𝘀: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     f"`𝗠𝘆 𝗣𝗲𝗿𝘂 𝗼𝘄𝗻𝗲𝗿`: {DEFAULTUSER}\n")
