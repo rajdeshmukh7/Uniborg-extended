7d4ec9baba444c58060fe20605d206c7108c42c1
rajdeshmukh7 <59872124+rajdeshmukh7@users.noreply.github.com>
2020-02-07 08:35:12 +0530

    Update tts.py

--- userbot/plugins/tts.py
+++ userbot/plugins/tts.py
@@ -1,7 +1,7 @@
 """ Google Text to Speech
 Available Commands:
-.tts LanguageCode as reply to a message
-.tts LangaugeCode | text to speak"""
+.ttss LanguageCode as reply to a message
+.ttss LangaugeCode | text to speak"""
 
 import asyncio
 import os
@@ -11,7 +11,7 @@ from gtts import gTTS
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("tts (.*)"))
+@borg.on(admin_cmd("ttss (.*)"))
 async def _(event):
     if event.fwd_from:
         return
